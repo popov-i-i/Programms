d9f61e11571f9de166e31456a1dd24a970cb514b
popov-i-i <61424820+popov-i-i@users.noreply.github.com>
2020-02-27 14:29:36 +0700

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,24 @@
+public class Main {
+    static float degree(int a, int b) {
+        if (b == 0){
+            return 1;
+        }
+        if (b < 0) {
+            b = b * (-1);
+            b -=  1;
+            float m = a * degree(a,b);
+            float result = 1/m;
+            return result;
+        } else {
+            b -= 1;
+           float m = a * degree(a,b);
+            return m;
+        }
+    }
+
+        public static void main(String[] args) {
+        int a = -6;
+        int b = 2;
+        System.out.println(degree(a,b));
+        }
+    }
