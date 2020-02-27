c233d0f2d20d501936edf2d7e2eec89c89e24640
popov-i-i <61424820+popov-i-i@users.noreply.github.com>
2020-02-27 15:53:23 +0700

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,5 +1,5 @@
 public class Main {
-    static void sort(int a[]) {
+    public static void sort(int a[]) {
         for (int i = 0; i < a.length - 1; i++) {
             for (int j = i + 1; j < a.length; j++) {
                 if (a[i] > a[j]) {
