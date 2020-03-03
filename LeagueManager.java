3b21cfe61003329c1772377b28739dee0c2fe8e4
popov-i-i <61424820+popov-i-i@users.noreply.github.com>
2020-03-03 16:16:43 +0700

    Delete LeagueManager.java

--- LeagueManager.java
+++
@@ -1,9 +0,0 @@
-public interface LeagueManager {
-    public void addPlayer(SC2Player player);
-    public void removePlayer(SC2Player player);
-    public SC2Player getPlayer(String name);
-    public SC2Player[] getAllPlayers();
-    public SC2Player[] getPlayers(League league);
-    public SC2Player[] getPlayers(Race race);
-    public void addPoints(String name, int points);
-}
