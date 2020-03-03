6cbdc1f847e73bf4fb9fd6901796fa789fbb4377
popov-i-i <61424820+popov-i-i@users.noreply.github.com>
2020-03-03 16:24:26 +0700

    Add files via upload

---
+++ Ex_2_2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void sort(int a[]) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int b = a[i];
+                    a[i] = a[j];
+                    a[j] = b;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 4, -1, 2, 1};
+        sort(a);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
